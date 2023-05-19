90d5d996b8a493ad08b48d0b2a4d7503e869740d
imak02 <asbinkhanal222@gmail.com>
2023-05-19 15:46:56 +0545

    Add event model

---
+++ backend/models/Event.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  details: { type: String },
+  image: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  location: { type: String, required: true },
+  capacity: { type: Number, required: true },
+  category: {
+    type: String,
+    enum: ["business", "sports", "technology", "education", "entertainment"],
+  },
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+module.exports = Event;
